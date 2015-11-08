3c85f171a178e9bdd1cf7a1ed305a6e91cc758cc
mzhang94 <mzhang94@mit.edu>
2015-11-07 22:48:50 -0500

    books

--- data_collect.py
+++ data_collect.py
@@ -1,6 +1,6 @@
 import urllib2
 from BeautifulSoup import BeautifulSoup
-import re, string
+import re, string, os
 
 class Book:
     def __init__(self, name):
@@ -13,7 +13,20 @@ class Book:
         soup = BeautifulSoup(page.read(), convertEntities=BeautifulSoup.HTML_ENTITIES)
         regex = re.compile('<!--\\n.*DisplayAds.*\\n.*-->')
         for div in soup.findAll("div", {"class" :"content_txt"}):
-            self.characters[div.get('id')] = regex.sub("", div.text)
+            text = regex.sub("", div.text)
+            text = text.split('\n')[1:]
+            text = '\n'.join(text)
+            text = text.encode('ascii', 'ignore')
+            self.characters[div.get('id')] = text
+
+    def writeToFile(self):
+        directory = 'books/%s' % self.name
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+        for name in self.characters:
+            f = open('%s/%s' % (directory, name), 'w')
+            f.write(self.characters[name])
+
 
 books = set([])
 for l in string.ascii_lowercase:
@@ -27,4 +40,8 @@ for l in string.ascii_lowercase:
         if not url == None and url.startswith("http://www.sparknotes.com/lit/"):
             book = url[30:]
             if book != "" and not book.startswith("index"):
+                print 'getting book %s' % book
                 books.add(book)
+                book = Book(book)
+                book.getCharactersFromWeb()
+                book.writeToFile()
